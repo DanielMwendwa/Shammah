398252147b4761e7b45d526b3d259a7c964cf4c0
Daniel Mwendwa <danielmwendwam@gmail.com>
2021-01-24 23:29:49 +0300

    update navbar name

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -11,6 +11,7 @@ import {
     USER_REGISTER_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_RESET,
     USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
 
@@ -55,7 +56,7 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     }
 };
 
-export const userUpdateProfileReducer = (state = { }, action) => {
+export const userUpdateProfileReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_UPDATE_PROFILE_REQUEST:
             return { loading: true };
@@ -63,6 +64,8 @@ export const userUpdateProfileReducer = (state = { }, action) => {
             return { loading: false, success: true, userInfo: action.payload };
         case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload };
+        case USER_UPDATE_PROFILE_RESET:
+            return {};
         default:
             return state;
     }
