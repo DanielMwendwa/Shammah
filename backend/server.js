d8a97e6f25bad119a8efce989ff722a9795b5561
Daniel Mwendwa <danielmwendwam@gmail.com>
2021-01-26 11:37:49 +0300

    rm typos

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -14,7 +14,7 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API is running...");
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes)
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
