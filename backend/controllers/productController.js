f462e6e9228a89ce6ee3e19268400c6025cb920e
Daniel Mwendwa <danielmwendwam@gmail.com>
2021-01-31 00:10:37 +0300

    format code

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,15 +28,15 @@ const getProductById = asyncHandler(async (req, res) => {
 // @route   DELETE /api/products/:id
 // @access  Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-  
+    const product = await Product.findById(req.params.id);
+
     if (product) {
-      await product.remove()
-      res.json({ message: 'Product removed' })
+        await product.remove();
+        res.json({ message: "Product removed" });
     } else {
-      res.status(404)
-      throw new Error('Product not found')
+        res.status(404);
+        throw new Error("Product not found");
     }
-  })
+});
 
 export { getProducts, getProductById, deleteProduct };
