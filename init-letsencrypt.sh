fa4d69906b5f9bce0ef959d5a31971526f9e0797
Daniel Mwendwa <danielmwendwam@gmail.com>
2021-02-12 16:55:46 +0300

    Enable staging mode if needed

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -85,7 +85,7 @@ case "$email" in
 esac
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [[ $STAGING == true ]] then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
