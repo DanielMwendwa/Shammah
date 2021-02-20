33a8157476c7d768567ea69b7b03064c940fa68e
Daniel Mwendwa <danielmwendwam@gmail.com>
2021-02-20 11:34:35 +0300

    enable order reset

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -20,6 +20,7 @@ import {
     ORDER_PAY_REQUEST,
     ORDER_PAY_RESET,
     ORDER_PAY_SUCCESS,
+    ORDER_CREATE_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -30,6 +31,8 @@ export const orderCreateReducer = (state = {}, action) => {
             return { loading: false, success: true, order: action.payload };
         case ORDER_CREATE_FAIL:
             return { loading: false, error: action.payload };
+        case ORDER_CREATE_RESET:
+            return {}
         default:
             return state;
     }
